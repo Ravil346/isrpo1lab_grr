3aa75e719decb616bee6661cf30787b1051367fe
Emil <n.emil.2710@gmail.com>
2022-10-18 15:48:20 +0300

    add button handler

--- isrpo1lab_grr/MainWindow.xaml.cs
+++ isrpo1lab_grr/MainWindow.xaml.cs
@@ -25,9 +25,10 @@ namespace isrpo1lab_grr
             InitializeComponent();
         }
 
-        private void Button_Click()
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            Window1 window1 = new Window1();
+            window1.Show();
         }
     }
 }
