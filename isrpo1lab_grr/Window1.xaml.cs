09f83f84769162cb48f717eef6ad134da7508f3a
Emil <n.emil.2710@gmail.com>
2022-10-18 15:38:03 +0300

    new window

--- isrpo1lab_grr/Window1.xaml.cs
+++ isrpo1lab_grr/Window1.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace isrpo1lab_grr
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click()
-        {
-
         }
     }
 }
